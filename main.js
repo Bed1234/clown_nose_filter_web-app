1381a94ac1d62388589a54ea1eab744d5658a6be
Bed1234 <76521252+Bed1234@users.noreply.github.com>
2021-09-03 20:11:00 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+nose_X= 0;
+nose_Y = 0;
 
 function setup() {
    canvas = createCanvas(300,300);
@@ -25,6 +27,12 @@ console.log('PoseNet is initialized');
 function draw(){
     image(video, 0, 0,300, 300);
 
+
+    fill(255, 0 , 0);
+    stroke(255, 0, 0);
+    circle(nose_X, nose_Y, 20);
+
+
 }
 
 
@@ -33,10 +41,15 @@ function gotPoses(results){
     if(results.length > 0 )
     {
          console.log(results);
+
+         nose_X = results[0].pose.nose.x;
+         nose_Y = results[0].pose.nose.y;
+
          console.log("nose x = " + results[0].pose.nose.x);
          console.log("nose y = " + results[0].pose.nose.y);
     }
 }
+
 
 function take_snapshot() {
     save('My_Filter_Image.png');
